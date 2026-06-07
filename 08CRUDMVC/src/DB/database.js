b4adf45d749a2e9eafe3444fbd11f960a8b551a5
GusSaman0 <gsamanot2400@alumno.ipn.mx>
2026-06-06 23:59:04 -0600

    08CRUDMVC, correcion de errores

---
+++ 08CRUDMVC/src/DB/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+//creamos la conexion
+const pool = mysql.createPool({
+    host : 'localhost',
+    user : 'root',
+    password : 'Pelu$apeluda1',
+    database : 'practicacrud',
+    waitForConnections : true,
+    connectionLimit : 10,
+    queueLimit : 0
+});
+
+//la exportamos para poder usarla
+module.exports = pool.promise();
